7bac8e1d32ab8b6d14b530a3587d8bf67e5cd1dd
Zigtecnologia-web <software@zigtecnologia.com>
2025-10-11 21:49:36 -0300

    First commit, Creating a Log Trait

--- App/Controllers/Api/UserController.php
+++ App/Controllers/Api/UserController.php
@@ -13,7 +13,6 @@ class UserController extends Controller
     public function __construct()
     {
         parent::__construct();
-
         //JwtAuthService::abortIfNotAuthenticated('Unauthorized');
     }
 
